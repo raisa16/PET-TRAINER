5af443d5addec38eeb9a9bbafb055e1f7b664816
Raisa <git>
2022-08-25 13:08:18 -0600

    Add index.html, main.js, images and css basic base.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+function dogData ()  {
+let petName = document.getElementById('petName').value;
+let months = document.getElementById('petMonths').value;
+let traningLevel = document.getElementById('traningLevel').value; 
+let ownerName = document.getElementById('ownerName').value; 
+let phone = document.getElementById('phone').value; 
+
+    console.log(petName);
+    return petName;
+  }
